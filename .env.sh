39369c6e8cb7654c6cb0f3f3ecbe49fa9a280380
Yash Nitin Virulkar <virulkaryashed@gmail.com>
2026-09-22 12:36:59 +0530

    init: basic staring with so101
    Signed-off-by: Yash Nitin Virulkar <virulkaryashed@gmail.com>

---
+++ .env.sh
@@ -0,0 +1,7 @@
+# source this before running anything in this project
+export PROJ="/home/viscous/Viscous/so101"
+export UV_CACHE_DIR="$PROJ/.caches/uv"
+export HF_HOME="$PROJ/.caches/hf"
+export MUJOCO_GL=egl        # headless rendering; falls back to osmesa
+export TOKENIZERS_PARALLELISM=false
+export UV_PYTHON_INSTALL_DIR="$PROJ/.caches/pythons"
